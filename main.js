f9f7f12dd7f60b3ef67f07b8d7f909bb56b0d1b4
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2021-09-01 20:37:57 -0500

    DOM selectores 01 09 2021

---
+++ main.js
@@ -0,0 +1,31 @@
+
+
+addEventListener("DOMContentLoaded", async(e)=>{
+    let peticion = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+    let data = await peticion.json();
+    console.log(data);
+
+    // let fragmen = document.createDocumentFragment();
+    // fragmen.append(document.body.children[0]);
+    // let div = fragmen.children[0].children;
+    
+    // for(let [id, value] of Object.entries(data.success)){
+    //     if(id==0){
+    //         div.myTitulo.insertAdjacentText('afterbegin', Object.values(value));
+    //     }else{
+    //         div.myParrafo.insertAdjacentText('afterbegin', Object.values(value));
+    //     }
+    // }
+    // console.log(data);
+    // document.body.insertAdjacentElement('afterbegin', fragmen.children[0]);
+
+
+
+    // let nombre = "Miguel";
+    // let fragmen = document.createDocumentFragment();
+    // let h1 = document.querySelector('h1');
+    // h1.innerText = `${nombre}`;
+    // fragmen.appendChild(h1);
+    // document.body.insertAdjacentElement('afterbegin', fragmen.children[0]);
+ 
+})
